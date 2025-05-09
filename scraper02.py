551814abf4c79eff2b6b8bff0d61ce14dd26e46d
Paras Gautam <paras.gautam@gmail.com>
2025-05-09 20:06:48 +0545

    xyz

--- scraper02.py
+++ scraper02.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import re
 import os
+from time import perf_counter
 
-class WikiTableScraper:
+class StockTableScraper:
     symbol = []
     market_price = []
     book_value = []
@@ -17,77 +17,77 @@ class WikiTableScraper:
     except FileNotFoundError as identifier:
         pass
     
+    def fetch_html_text(self, url):
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            return response
+        else:
+            print("Page is not responding...")
 
-
-    def fetch(self, url):
-        return requests.get(url)
-
-
-    def parse(self, html):
+    def parse_html_text(self, html):
         soup = BeautifulSoup(html, 'lxml')
         table = soup.find("table", attrs={"class":"table table-hover live-trading sortable"})
-        # thead = table.find("thead")
-        # header = thead.find("th")
-        # self.symbol.append(header.text)
 
-        tbody = table.find("tbody")
-        trows = tbody.find_all("tr")
+        trows = table.find("tbody").find_all("tr")
+        # trows = tbody.find_all("tr")
 
         for row in trows:
             company = row.find('td')
             self.symbol.append(company.text)
         
-        self.toCompanyLink(self.symbol)
+        self.create_company_urls(self.symbol)
 
-    def toCompanyLink(self, symbol):  
+    def create_company_urls(self, symbol):
         for company_name in self.symbol:
-            company_url = 'https://merolagani.com/CompanyDetail.aspx?symbol='
-            company_url = company_url + company_name
+            company_url = 'https://merolagani.com/CompanyDetail.aspx?symbol=' + company_name
+            # company_url = company_url + company_name
             self.company_urls.append(company_url)
         
-        self.parseCompany(self.company_urls, symbol)
+        self.get_company_details(self.company_urls, symbol)
 
 
-    def parseCompany(self, company_urls, company_name):
+    def get_company_details(self, company_urls, company_name):
         for each_url in self.company_urls:
-            response = self.fetch(each_url)
+            response = self.fetch_html_text(each_url)
             soup = BeautifulSoup(response.content, 'lxml')
             table = soup.find('table', attrs={'class': 'table table-striped table-hover table-zeromargin'})
 
             mkt_price = table.find(id='ctl00_ContentPlaceHolder1_CompanyDetail1_lblMarketPrice')
-            self.market_price.append(mkt_price.text.strip().replace(',', ''))
+            if mkt_price is not None:
+                self.market_price.append(mkt_price.text.strip().replace(',', ''))
+                bok_value = table.find_all('td')
+                self.book_value.append(bok_value[11].text.strip().replace(',', ''))
+            else:
+                print("Element not found!")
 
-            bok_value = table.find_all('td')
-            self.book_value.append(bok_value[11].text.strip().replace(',', ''))
-
-        self.toDataframe(company_name, self.market_price, self.book_value)
-            
-        # print(self.market_price)
-        # print('-' * 100)
-        # print(self.book_value)
+        self.create_dataframe(company_name, self.market_price, self.book_value)
      
-
     def toText(self, symbol_table):
         fileObj = open('table.txt', 'a+', encoding='utf-8')
         fileObj.write(symbol_table)
         fileObj.write('\n')
 
-
-    def toDataframe(self, company_name, market_price, book_value):
+    def create_dataframe(self, company_name, market_price, book_value):
         df = pd.DataFrame(list(zip(company_name, market_price, book_value)), columns=['symbol', 'marketPrice', 'bookValue'])
         df['marketPrice'] = df['marketPrice'].astype(float)
         df['bookValue'] = df['bookValue'].astype(float)
-        print(df[(df['marketPrice']<=df['bookValue']+40) & df['bookValue'] != 0])
-        # print(df)
-
+        df['diff'] = df['marketPrice'] - df['bookValue']
+        print(df[(df['marketPrice']<=df['bookValue']+100) & df['bookValue'] != 0].sort_values(by='diff', ascending=False))
 
     def run(self):
-        response = self.fetch(self.url)
+        response = self.fetch_html_text(self.url)
         # print(response)
-        self.parse(response.text)
+        self.parse_html_text(response.text)
         # self.toDataframe()
 
 
 if __name__ == '__main__':
-    scraper = WikiTableScraper()
-    scraper.run()
+    t1_start = perf_counter()
+    scraper = StockTableScraper().run()
+    # scraper.run()
+    t2_stop = perf_counter()
+
+    print("-----------------------------------------")
+
+    print("Time diff: {0}".format(t2_stop - t1_start))
